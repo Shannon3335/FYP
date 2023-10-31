362c760c5eddbe4c9c32db62b891209fcdb62048
G00398070 <G00398070@atu.ie>
2023-10-31 16:05:43 +0000

    got the default code given by open ai working

--- app/page.js
+++ app/page.js
@@ -2,8 +2,10 @@
 
 import { AddUser } from "/firebase/AddUser.js"
 import { ReadUser } from "/firebase/ReadUser.js"
+import { useCompletion } from "ai/react"
 
 export default function Home() {
+  const { completion, input, handleInputChange, handleSubmit} = useCompletion();
 
   const addUser = async () => {
     console.log("Calling the true Add user function")
@@ -29,6 +31,21 @@ export default function Home() {
         <div><button className='border-white border-2' onClick={addUser}>Add User</button>
         <div><button className='radius-2 boredr-green-400 border-4' onClick={readUser}>ReadUser</button></div>
       </div>
+      <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2 dark:text-black"
+          value={input}
+          placeholder="Describe your business..."
+          onChange={handleInputChange}
+        />
+      </form>
+      {completion ? (
+        <div className="whitespace-pre-wrap my-4">{completion}</div>
+      ) : (
+        <div>Enter a business description and click enter to generate slogans.</div>
+      )}
+    </div>
 
 </div>
     </main>
