4c1df20ecabd460fdc9e39d6efb899319d1bf973
G00398070 <G00398070@atu.ie>
2023-11-30 19:06:37 +0000

    created auth object in firebase.js

--- app/functions/firebase/FirebaseApp.js
+++ app/functions/firebase/FirebaseApp.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -31,5 +32,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Initialise Firebase Authentication 
+export const auth = getAuth(app)
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
