9fad61a873d97c7550f8468401fc743815361988
G00398070 <G00398070@atu.ie>
2024-01-24 19:29:20 +0000

    getting userDetails returned from firebase
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- app/functions/firebase/Signin.js
+++ app/functions/firebase/Signin.js
@@ -2,17 +2,19 @@ import { signInWithEmailAndPassword } from "firebase/auth"
 import { auth } from "./FirebaseApp"
 
 const Signin = async (email, password) => {
-  signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password)
     const user = userCredential.user
-    console.log("Signed in: " + JSON.stringify(user))
+    // console.log("Signed in: " + JSON.stringify(user))
     const uid = user.uid
-    console.log("My uid :" + uid)
-  })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log("Error code: " + errorCode + "Error message: " + errorMessage)
-    })
+    // console.log("My uid :" + uid)
+    return { userID: uid }
+  }
+  catch (e) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log("Error code: " + errorCode + "Error message: " + errorMessage)
+  }
 }
 
 export default Signin
