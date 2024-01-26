c6024f697fae3b2d1f29f4acf0522dad78b917a1
G00398070 <G00398070@atu.ie>
2024-01-26 10:48:27 +0000

    changed from useAtom to useAtomValue in quiz page
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- app/quiz/page.js
+++ app/quiz/page.js
@@ -8,10 +8,10 @@ import { useEffect, useState } from "react"
 import ConvertToQuizObjects from "../functions/convertToQuizObject"
 import userAtom from "../../atoms/userAtom"
 // import { industryAndFieldAtom } from "../../atoms/userAtom"
-import { useAtom } from "jotai"
+import { useAtomValue } from "jotai"
 
 export default function Quiz() {
-  const [user, modifyUser] = useAtom(userAtom)
+  const user = useAtomValue(userAtom)
   // const [jobAndIndustry, modJandI] = useAtom(industryAndFieldAtom)
   const [quizArray, setquizArray] = useState(null)
   const router = useRouter()
@@ -24,31 +24,31 @@ export default function Quiz() {
   })
   useEffect(() => console.log(quizArray), [quizArray])
 
-  useEffect(()=>{
+  useEffect(() => {
     if (user.userName === '') {
       router.push("/")
     }
   })
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
-            <div>${JSON.stringify(user)}</div>
-            <form onSubmit={handleSubmit}>
-              <input
-                className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2 dark:text-black"
-                value={input}
-                placeholder="Enter your job title for the questions"
-                onChange={handleInputChange}
-              />
-            </form>
-            {completion ? (
-              <div className="whitespace-pre-wrap my-4">{completion}</div>
-            ) : (
-              <div>Enter your job title </div>
-            )}
-          </div>
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
+          <div>${JSON.stringify(user)}</div>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2 dark:text-black"
+              value={input}
+              placeholder="Enter your job title for the questions"
+              onChange={handleInputChange}
+            />
+          </form>
+          {completion ? (
+            <div className="whitespace-pre-wrap my-4">{completion}</div>
+          ) : (
+            <div>Enter your job title </div>
+          )}
         </div>
+      </div>
     </main>
   )
 }
