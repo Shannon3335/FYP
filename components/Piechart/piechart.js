a14880e6c617575a06666877410c49825b3f90f7
G00398070 <G00398070@atu.ie>
2024-01-31 18:19:46 +0000

    comitting code. Changed pie component to use props, not tested on quiz page

--- components/Piechart/piechart.js
+++ components/Piechart/piechart.js
@@ -1,20 +1,28 @@
-//TESTING PAGE FOR PROOF OF CONCEPT OF THE PIE CHART
 import React from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Pie } from 'react-chartjs-2'
 
+//Register only what you need from chartjs for tree shakability (reduces code size when deploying)
 ChartJS.register(ArcElement, Tooltip, Legend)
 
 const TestPie = () => {
   const data = {
-    labels: ['Correct', 'Incorrect'],
+    // labels: ['Correct', 'Incorrect'],
+    labels: props.labels,
     datasets: [
       {
-        label: '# of Votes',
-        data: [12, 19],
-        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
-        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
-        borderWidth: 1,
+        // label: '# of Votes',
+        // data: [12, 19],
+        data: props.data,
+
+        // backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
+        backgroundColor: props.bgcolor,
+
+        // borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
+        borderColor: props.borderColor,
+
+        // borderWidth: 1,
+        borderWidth: props.borderWidth,
       },
     ],
   }
