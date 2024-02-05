b175bbb1766a32a709b15b3bb7a3d12762f0aba0
G00398070 <G00398070@atu.ie>
2024-02-05 17:25:30 +0000

    Configured prettier using prettier rc file, now works with any ide

--- app/page.js
+++ app/page.js
@@ -19,39 +19,7 @@ export default function Home() {
     router.push('/login')
   }
   return (
-    <main
-      id='MainContainer'
-      className='bg-gradient-to-b from-slate-900 via-purple-900 to-pink-500 flex flex-col min-h-screen h-screen w-full flex-nowrap justify-between'
-    >
-      <div
-        id='NavBar'
-        className='flex flex-row justify-between w-full p-3 font-mono text-sm '
-      >
-        <div id='Logo' className=''>
-          PrepME
-        </div>
-        <div id='MiddleLinks' className='flex flex-row'>
-          <button className=' me-8' onClick={toGithub}>
-            Github
-          </button>
-          <div className=' me-8'>Contact</div>
-          <div className=''>Design Rules</div>
-        </div>
-        <div id='SignInButtons' className='flex flex-row'>
-          <button
-            className='bg-slate-600 hover:bg-blue-700 rounded-lg me-6'
-            onClick={toSignup}
-          >
-            Signup
-          </button>
-          <button
-            className='bg-slate-600 hover:bg-blue-700 rounded-lg'
-            onClick={toLogin}
-          >
-            Login
-          </button>
-        </div>
-      </div>
+    <main>
       <div
         id='MainBody'
         className='flex flex-col w-full min-h-full h-full '
