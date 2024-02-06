a1dddcc8eae2d16f7367cc0d3d78d80d7a989c1f
G00398070 <G00398070@atu.ie>
2024-02-06 13:49:55 +0000

    finished setting up shadcn, move to other branch to work on homepage
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 'use client'
 import { useRouter } from 'next/navigation'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 export default function Home() {
   const router = useRouter()
@@ -22,8 +24,40 @@ export default function Home() {
     <main>
       <div
         id='MainBody'
-        className='flex flex-col w-full min-h-full h-full '
-      ></div>
+        className='flex h-full min-h-full w-full flex-col '></div>
+      <div>
+        <div
+          id='NavBar'
+          className=' space-between flex w-full flex-row justify-between p-3 font-mono text-sm'>
+          <div className='p-2 text-lg '>PREPME</div>
+          <section
+            id='MiddleLinks'
+            className='w-1/4 flex flex-row justify-between'>
+            <Button asChild>
+              <Link href={'https://github.com/Shannon3335/FYP'} target='_blank'>
+                Github
+              </Link>
+            </Button>
+
+            <Button asChild variant='link'>
+              <Link href={'/quiz'}>Quiz</Link>
+            </Button>
+
+            <Button asChild variant='link'>
+              <Link
+                href={
+                  'https://docs.google.com/document/d/e/2PACX-1vTqBRxYfns9hSk6zSf4PjvHcnB6FHvNBFE6nIMLnArgitQNd7Ul1jwIw9oOi2Imdg/pub'
+                }
+                target='_blank'>
+                Docs
+              </Link>
+            </Button>
+          </section>
+          <Button asChild variant='ghost' onClick={toSignup}>
+            <Link href={'/signup'}>Signup</Link>
+          </Button>
+        </div>
+      </div>
       <div className='flex '>asdfsfd</div>
     </main>
   )
