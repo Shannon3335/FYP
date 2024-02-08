51ff26a8a1fbc6be7beb02ceb930a1001d797460
G00398070 <G00398070@atu.ie>
2024-02-08 09:54:50 +0000

    comitting current latest version
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- app/page.js
+++ app/page.js
@@ -3,6 +3,12 @@ import { useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import ThemeSwitcher from '@/components/theme-switcher'
+import {
+  EnvelopeOpenIcon,
+  GitHubLogoIcon,
+  HandIcon,
+} from '@radix-ui/react-icons'
+import { Separator } from '@/components/ui/separator'
 
 export default function Home() {
   const router = useRouter()
@@ -22,29 +28,32 @@ export default function Home() {
     router.push('/login')
   }
   return (
-    <main>
-      <div
-        id='MainBody'
-        className='flex h-full min-h-full w-full flex-col '></div>
-      <div>
+    <main className='flex-column min-h-screen min-w-full'>
+      <div id='header' className='flex flex-row justify-between py-3'>
+        <div id='logo' className=' text-lg '>
+          <text>PREPME</text>
+          <HandIcon className='ml-5 h-4 w-4' />
+        </div>
         <div
-          id='NavBar'
-          className=' space-between flex w-full flex-row justify-between p-3 font-mono text-sm'>
-          <div className='p-2 text-lg '>PREPME</div>
-          <section
-            id='MiddleLinks'
-            className='flex w-1/4 flex-row justify-between'>
-            <Button asChild>
+          id='MiddleLinks'
+          className='flex w-1/4 flex-row items-center font-mono text-sm'>
+          <Button asChild variant='ghost'>
+            <span>
+              <GitHubLogoIcon className='mr-2 h-4 w-4' />
               <Link href={'https://github.com/Shannon3335/FYP'} target='_blank'>
                 Github
               </Link>
-            </Button>
+            </span>
+          </Button>
+          <Separator orientation='vertical' className='mx-2' />
+          <Button asChild variant='ghost'>
+            <Link href={'/login'}>login</Link>
+          </Button>
+          <Separator orientation='vertical' className='mx-2' />
 
-            <Button asChild variant='link'>
-              <Link href={'/login'}>login</Link>
-            </Button>
-
-            <Button asChild variant='link'>
+          <Button asChild variant='ghost'>
+            <span>
+              <EnvelopeOpenIcon className='mr-2 h-4 w-4' />
               <Link
                 href={
                   'https://docs.google.com/document/d/e/2PACX-1vTqBRxYfns9hSk6zSf4PjvHcnB6FHvNBFE6nIMLnArgitQNd7Ul1jwIw9oOi2Imdg/pub'
@@ -52,18 +61,20 @@ export default function Home() {
                 target='_blank'>
                 Docs
               </Link>
-            </Button>
-          </section>
-          <div className='flex flex-row justify-between w-1/6'>
+            </span>
+          </Button>
+        </div>
+        <div
+          id='CornerButtons'
+          className='w-1/12 flex-row items-center justify-between'>
           <Button asChild variant='ghost' onClick={toSignup}>
             <Link href={'/signup'}>Signup</Link>
           </Button>
-          <ThemeSwitcher/>
-          </div>
-          
+          <ThemeSwitcher />
         </div>
       </div>
-      <div className='flex '>asdfsfd</div>
+
+      <div className='flex  '>asdfsfd</div>
     </main>
   )
 }
