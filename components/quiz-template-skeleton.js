5efd8411994cdede676dafe8aba3e14ac82d9aa5
G00398070 <G00398070@atu.ie>
2024-02-18 21:02:57 +0000

    created quiz skeletion file, currently working on this
    Signed-off-by: G00398070 <G00398070@atu.ie>

---
+++ components/quiz-template-skeleton.js
@@ -0,0 +1,15 @@
+import { Card, CardHeader } from './ui/card'
+
+const QuizTemplateSkeleton = () => {
+  return (
+    <div id='main-container'>
+      <Card>Hello</Card>
+      <Card>Q1</Card>
+      <Card>Q2</Card>
+      <Card>Q3</Card>
+      <Card>Q4</Card>
+    </div>
+  )
+}
+
+export default QuizTemplateSkeleton
