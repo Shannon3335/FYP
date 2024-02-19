cc7d0bfafac9be46a29db48e7e97fa7d9486369b
G00398070 <G00398070@atu.ie>
2024-02-19 18:21:01 +0000

    created basic layout, need to add next button and make it look better
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- components/quiz-template.js
+++ components/quiz-template.js
@@ -1,6 +1,58 @@
+import { useState } from 'react'
+import { Button } from './ui/button'
+import { Card } from './ui/card'
+
 const QuizTemplate = (props) => {
-  let question = props.quizArray
-  return <div></div>
+  let quizArray = [
+    {
+      question: 'What is the capital of France?',
+      options: ['Berlin', 'Madrid', 'Paris', 'Rome'],
+      answer: 'Paris',
+    },
+    {
+      question: 'Which planet is known as the Red Planet?',
+      options: ['Venus', 'Mars', 'Jupiter', 'Saturn'],
+      answer: 'Mars',
+    },
+    {
+      question: 'Who painted the Mona Lisa?',
+      options: ['Leonardo da Vinci', 'Pablo Picasso', 'Vincent van Gogh', 'Michelangelo'],
+      answer: 'Leonardo da Vinci',
+    },
+  ]
+  const [isQuizOver, setQuizOver] = useState(false)
+  const [activeQuestionNo, setActiveQuestionNo] = useState(0)
+  const [selectedOption, setSelectedOption] = useState(null)
+
+  return (
+    <div
+      id='main-container'
+      className='flex min-h-screen min-w-full flex-col content-between items-start lg:items-center '>
+      {!isQuizOver ? (
+        <>
+          <div id='question' className='min-h-full w-full py-6 text-2xl lg:w-4/5 '>
+            {/* <Card>{props.quizArray[activeQuestionNo].question}</Card> */}
+            <text className='text-lg'>Q{activeQuestionNo +1}</text>
+            <Card className='lg:text-center'>{quizArray[activeQuestionNo].question}</Card>
+          </div>
+          <div
+            id='options'
+            className='flex w-full flex-col items-start space-y-10 lg:flex-row lg:flex-wrap lg:items-end lg:justify-around'>
+            {/* {props.quizArray[activeQuestionNo].options.map((option, index) => ( */}
+            {quizArray[activeQuestionNo].options.map((option, index) => (
+              <Button
+                variant='mcq'
+                key={option}
+                onClick={() => setSelectedOption(option)}
+                className='min-h-fit w-full lg:w-5/12 lg:flex-none'>
+                {option}
+              </Button>
+            ))}
+          </div>
+        </>
+      ) : null}
+    </div>
+  )
 }
 
 export default QuizTemplate
--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -15,6 +15,7 @@ const buttonVariants = cva(
         secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        mcq: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 focus:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500',
       },
       size: {
         default: 'h-9 px-4 py-2',
