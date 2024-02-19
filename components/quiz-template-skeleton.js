1b62bfa35d97db0135727687a3019d3b1abdbdb4
G00398070 <G00398070@atu.ie>
2024-02-19 10:53:37 +0000

    downloaded shadcn skeleton
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- components/quiz-template-skeleton.js
+++ components/quiz-template-skeleton.js
@@ -1,14 +1,28 @@
-import { Card, CardHeader } from './ui/card'
-
+import { Card } from './ui/card'
+import { Skeleton } from './ui/skeleton'
 const QuizTemplateSkeleton = () => {
   return (
-    <div id='main-container' className='flex flex-row '>
-      <Card>Hello</Card>
-      <div id='options'>
-        <Card>Q1</Card>
-        <Card>Q2</Card>
-        <Card>Q3</Card>
-        <Card>Q4</Card>
+    <div
+      id='main-container'
+      className='flex min-h-screen min-w-full flex-col content-between items-start lg:items-center '>
+      <div id='question' className=' min-h-full w-full py-6 lg:w-1/2'>
+        <Card>
+          <Skeleton className='h-32' />
+        </Card>
+      </div>
+      <div id='options' className='lg:wrap flex min-w-full flex-col items-start space-y-5 lg:flex-row lg:space-x-5'>
+        <Card className='min-h-fit w-full lg:w-1/2'>
+          <Skeleton className='h-5  rounded-md ' />
+        </Card>
+        <Card className='min-h-fit w-full  lg:w-1/2'>
+          <Skeleton className='h-5 rounded-md  ' />
+        </Card>
+        <Card className='min-h-fit w-full  lg:w-1/2'>
+          <Skeleton className='h-5 rounded-md  ' />
+        </Card>
+        <Card className='min-h-fit w-full  lg:w-1/2'>
+          <Skeleton className='h-5 rounded-md  ' />
+        </Card>
       </div>
     </div>
   )
