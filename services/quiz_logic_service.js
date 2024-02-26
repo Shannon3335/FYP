56c2389a49f671b5500621641c880869f9322e5a
G00398070 <G00398070@atu.ie>
2024-02-26 16:12:25 +0000

    modifying quizservice, not sure if it will work
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- services/quiz_logic_service.js
+++ services/quiz_logic_service.js
@@ -1,7 +1,34 @@
 class QuizLogicService {
-  constructor(quizData) {}
+  constructor(quizArray) {
+    this.quizArray = quizArray
+    this.isQuizOver = false
+    this.activeQuestionNo = 0
+    this.selectedOption = null
+    this.selectedIndex = null
+    this.isLastQuestion = false
+    this.result = {
+      score: 0,
+      correctAnswers: 0,
+      wrongAnswers: 0,
+    }
+  }
 
-  verifyAnswer() {}
+  verifyAnswer() {
+    if (this.selectedOption == this.quizArray[this.activeQuestionNo].answer) {
+      console.log('correct answer')
+      result = {
+        ...this.result,
+        score: this.result.score + 5,
+        correctAnswers: this.correctAnswers + 1,
+      }
+    } else {
+      console.log('incorrect answer')
+      this.result = {
+        ...this.result,
+        wrongAnswers: this.result.wrongAnswers + 1,
+      }
+    }
+  }
 
   getSelectedOption() {}
 
