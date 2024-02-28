9374b7791f8215445208bd7c4744d1213ae6721c
G00398070 <G00398070@atu.ie>
2024-02-28 16:25:59 +0000

    committing for now, not wroking
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- atoms/quizAtom.js
+++ atoms/quizAtom.js
@@ -1,15 +1,21 @@
 import { atom } from 'jotai'
-
 const quizDataAtom = atom(null)
 const isQuizOver = false
 const activeQuestionNo = atom(0)
 const selectedOption = atom(null)
 const selectedIndex = atom(null)
 const isLastQuestion = atom(false)
+const setSelectedIndex = atom(null)
+
 const result = atom({
   score: 0,
   correctAnswers: 0,
   wrongAnswers: 0,
+})
+
+const handleOptionSelect = atom(null, (get, set, payload) => {
+  setSelectedOption(payload.option)
+  setSelectedIndex(payload.index)
 })
 
 export { quizDataAtom, isQuizOver, activeQuestionNo, selectedIndex, selectedOption, isLastQuestion, result }
--- services/quiz_logic_service.js
+++ services/quiz_logic_service.js
@@ -1,27 +1,26 @@
-import { activeQuestionNo, quizDataAtom, result, selectedOption } from '@/atoms/quizAtom'
+import { activeQuestionNo, quizDataAtom, result, selectedIndex, selectedOption } from '@/atoms/quizAtom'
 import { useAtom, useAtomValue, useSetAtom } from 'jotai'
 
-class QuizLogicService {
-  constructor() {}
+const verifyAnswer = () => {
+  const [selectedOptionValue, setSelectedOption] = useAtom(selectedOption)
+  const activeQuestionNoValue = useAtomValue(activeQuestionNo)
+  const quizDataAtomValue = useAtomValue(quizDataAtom)
+  const setResult = useSetAtom(result)
+  const setSelectedIndex = useSetAtom(selectedIndex)
 
-  verifyAnswer() {
-    const [selectedOptionValue, setSelectedOption] = useAtom(selectedOption)
-    const activeQuestionNoValue = useAtomValue(activeQuestionNo)
-    const quizDataAtomValue = useAtomValue(quizDataAtom)
-    const setResult = useSetAtom(result)
-
-    if (selectedOptionValue == quizDataAtomValue.answer) {
-      console.log('correct answer')
-      setResult((prev) => ({
-        ...prev,
-        score: prev.score + 5,
-        correctAnswers: prev.correctAnswers + 1,
-      }))
-    } else {
-      console.log('incorrect answer')
-      setResult((prev) => ({ ...prev, wrongAnswers: prev.wrongAnswers + 1 }))
-    }
+  if (selectedOptionValue == quizDataAtomValue.answer) {
+    console.log('correct answer')
+    setResult((prev) => ({
+      ...prev,
+      score: prev.score + 5,
+      correctAnswers: prev.correctAnswers + 1,
+    }))
+  } else {
+    console.log('incorrect answer')
+    setResult((prev) => ({ ...prev, wrongAnswers: prev.wrongAnswers + 1 }))
   }
+  setSelectedOption(null)
+  setSelectedIndex(null)
 }
 
-export default QuizLogicService
+export { verifyAnswer }
