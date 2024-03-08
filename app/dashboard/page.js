6b2a05f5c2dfbf2e148642618bc7c52abc1f95cf
G00398070 <G00398070@atu.ie>
2024-03-08 13:22:07 +0000

    added quiz button to dashboard
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,12 @@
 import DashboardDrawer from '@/components/dashboardDrawer'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 const Dashboard = () => {
   return (
     <main>
+      <Button asChild variant='secondary'>
+        <Link href={'/quiz'}>Take Quiz</Link>
+      </Button>
       <div>
         <DashboardDrawer />
       </div>
