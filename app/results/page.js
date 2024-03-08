6b11927281243106aa1f1deea53cce21df445135
G00398070 <G00398070@atu.ie>
2024-03-08 12:56:22 +0000

    remove hard coded results
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- app/results/page.js
+++ app/results/page.js
@@ -15,125 +15,6 @@ const Results = () => {
   const score = result.score
 
   //Set these values below for testing purposes
-  useEffect(() => {
-    setQuizArray([
-      {
-        question: 'What is the primary goal of a Tech Consultant in a consulting firm?',
-        options: [
-          'To write code for clients',
-          'To analyze business processes and recommend technical solutions',
-          'To create marketing strategies for tech products',
-          "To manage the company's finances",
-        ],
-        answer: 'To analyze business processes and recommend technical solutions',
-        explanation:
-          'Tech Consultants in consulting firms primarily focus on analyzing business processes and providing technical solutions to improve efficiency and effectiveness.',
-      },
-      {
-        question: 'Which of the following is NOT a common responsibility of a Tech Consultant?',
-        options: [
-          'Managing client relationships',
-          'Developing software applications',
-          'Designing marketing campaigns',
-          'Providing technical expertise to clients',
-        ],
-        answer: 'Designing marketing campaigns',
-        explanation:
-          'Tech Consultants typically focus on providing technical expertise, developing solutions, and managing client relationships, rather than designing marketing campaigns.',
-      },
-      {
-        question: 'What is a key challenge that Tech Consultants often face in consulting projects?',
-        options: [
-          'Lack of technical knowledge',
-          'Limited client interaction',
-          'Difficulty in understanding business requirements',
-          'Overestimating project timelines',
-        ],
-        answer: 'Difficulty in understanding business requirements',
-        explanation:
-          'Tech Consultants may face challenges in understanding the unique business requirements of each client, which can impact the success of the project.',
-      },
-      {
-        question: 'Why is effective communication crucial for a Tech Consultant in consulting projects?',
-        options: [
-          'To show off technical knowledge',
-          'To confuse the client with jargon',
-          'To ensure client requirements are understood and met',
-          'To avoid interacting with stakeholders',
-        ],
-        answer: 'To ensure client requirements are understood and met',
-        explanation:
-          'Effective communication is essential for a Tech Consultant to clarify client requirements, align expectations, and ensure the successful implementation of technical solutions.',
-      },
-      {
-        question: 'Which of the following skills is most important for a Tech Consultant?',
-        options: ['Public speaking', 'Coding in multiple programming languages', 'Graphic design', 'Time management'],
-        answer: 'Time management',
-        explanation:
-          'Time management is crucial for a Tech Consultant to meet project deadlines, prioritize tasks effectively, and deliver high-quality technical solutions to clients.',
-      },
-      {
-        question: 'What is the primary goal of a Tech Consultant in a consulting firm?',
-        options: [
-          'To write code for clients',
-          'To analyze business processes and recommend technical solutions',
-          'To create marketing strategies for tech products',
-          "To manage the company's finances",
-        ],
-        answer: 'To analyze business processes and recommend technical solutions',
-        explanation:
-          'Tech Consultants in consulting firms primarily focus on analyzing business processes and providing technical solutions to improve efficiency and effectiveness.',
-      },
-      {
-        question: 'Which of the following is NOT a common responsibility of a Tech Consultant?',
-        options: [
-          'Managing client relationships',
-          'Developing software applications',
-          'Designing marketing campaigns',
-          'Providing technical expertise to clients',
-        ],
-        answer: 'Designing marketing campaigns',
-        explanation:
-          'Tech Consultants typically focus on providing technical expertise, developing solutions, and managing client relationships, rather than designing marketing campaigns.',
-      },
-      {
-        question: 'What is a key challenge that Tech Consultants often face in consulting projects?',
-        options: [
-          'Lack of technical knowledge',
-          'Limited client interaction',
-          'Difficulty in understanding business requirements',
-          'Overestimating project timelines',
-        ],
-        answer: 'Difficulty in understanding business requirements',
-        explanation:
-          'Tech Consultants may face challenges in understanding the unique business requirements of each client, which can impact the success of the project.',
-      },
-      {
-        question: 'Why is effective communication crucial for a Tech Consultant in consulting projects?',
-        options: [
-          'To show off technical knowledge',
-          'To confuse the client with jargon',
-          'To ensure client requirements are understood and met',
-          'To avoid interacting with stakeholders',
-        ],
-        answer: 'To ensure client requirements are understood and met',
-        explanation:
-          'Effective communication is essential for a Tech Consultant to clarify client requirements, align expectations, and ensure the successful implementation of technical solutions.',
-      },
-      {
-        question: 'Which of the following skills is most important for a Tech Consultant?',
-        options: ['Public speaking', 'Coding in multiple programming languages', 'Graphic design', 'Time management'],
-        answer: 'Time management',
-        explanation:
-          'Time management is crucial for a Tech Consultant to meet project deadlines, prioritize tasks effectively, and deliver high-quality technical solutions to clients.',
-      },
-    ])
-    setResults({
-      score: 15,
-      correctAnswers: 19,
-      wrongAnswers: 5,
-    })
-  }, [])
 
   const piechartProps = {
     labels: ['Incorrect', 'Correct'],
@@ -168,7 +49,7 @@ const Results = () => {
                   lg:justify-evenly '>
                     {questionObj.options.map((option, optionIndex) => (
                       <div key={optionIndex}>
-                        {optionIndex + 1 + ')'}
+                        {optionIndex + 1 + ') '}
                         {option}
                       </div>
                     ))}
