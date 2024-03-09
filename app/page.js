de086ddd264f63d986a7cccfa7c47ec31e87b21e
G00398070 <G00398070@atu.ie>
2024-03-09 09:20:01 +0000

    removed useless imports
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- app/page.js
+++ app/page.js
@@ -1,10 +1,4 @@
 'use client'
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import ThemeSwitcher from '@/components/theme-switcher'
-import { EnvelopeOpenIcon, GitHubLogoIcon, HandIcon } from '@radix-ui/react-icons'
-import { Separator } from '@/components/ui/separator'
-import TakeQuizModal from '@/components/take-quiz-modal'
 import Header from '@/components/header'
 
 export default function Home() {
