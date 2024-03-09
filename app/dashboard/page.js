bb3c4355da4f09da04e861111fac8664dda89020
G00398070 <G00398070@atu.ie>
2024-03-09 10:06:35 +0000

    usign header in my page
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,9 +1,14 @@
+'use client'
+
 import DashboardDrawer from '@/components/dashboardDrawer'
+import Header from '@/components/header'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+
 const Dashboard = () => {
   return (
-    <main>
+    <main className='flex h-screen w-full flex-col'>
+      <Header />
       <Button asChild variant='secondary'>
         <Link href={'/quiz'}>Take Quiz</Link>
       </Button>
