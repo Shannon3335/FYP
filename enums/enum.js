ebbf570c9c9390abb454a2254cff49946a20e321
G00398070 <G00398070@atu.ie>
2024-03-09 09:07:44 +0000

    added documentation to enum code
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- enums/enum.js
+++ enums/enum.js
@@ -2,6 +2,11 @@
 //Created using code from https://dmitripavlutin.com/javascript-enum/
 //Call this function in other files to create a proxied enum
 
+/*
+A proxy is exactly what it says it is, a proxy on an object. I create 
+logic that intercepts someone trying to get or set the value of a the
+variable that I create a Proxy for.
+*/
 const Enum = (baseEnum) => {
   return new Proxy(baseEnum, {
     get(target, prop) {
