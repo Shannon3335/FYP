91bc10489b1917452a9293f7677dd22ec07d2c93
G00398070 <G00398070@atu.ie>
2024-03-10 22:59:19 +0000

    created hook for transitions with tailwind
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,20 +1,28 @@
 'use client'
-
-import DashboardDrawer from '@/components/dashboardDrawer'
-import Header from '@/components/header'
-import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import Header from '@/components/header'
+import DashboardDrawer from '@/components/dashboardDrawer'
+import { Button } from '@/components/ui/button'
+import useRandomTextWithStyle from '@/services/transitionHook'
 
 const Dashboard = () => {
+  const transitionDuration = 3000
+  const ob = {
+    'take quiz': 'text-semibold text-red-500 p-15',
+    'hello there': 'text-semibold text-white p-15',
+    'beep boop': 'text-extrabold text-blue-400 p-2',
+  }
+  const { text, style } = useRandomTextWithStyle(transitionDuration, ob)
+
   return (
     <main className='flex h-screen w-full flex-col'>
       <Header />
       <Button asChild variant='secondary'>
-        <Link href={'/quiz'}>Take Quiz</Link>
+        <Link href='/quiz'>
+          <div className={`transition-all duration-500 ease-in-out ${style}`}>{text}</div>
+        </Link>
       </Button>
-      <div>
-        <DashboardDrawer />
-      </div>
+      <DashboardDrawer />
     </main>
   )
 }
--- services/transitionHook.js
+++ services/transitionHook.js
@@ -1,3 +1,4 @@
+'use client'
 import { useState, useEffect, useCallback } from 'react'
 
 const useRandomTextWithStyle = (transitionDuration, ob) => {
