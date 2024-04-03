1171d8dad040edf4065f687e79ed7a7c1856343c
G00398070 <G00398070@atu.ie>
2024-04-03 08:44:21 +0100

    changed dashboard icon
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,25 +1,14 @@
 'use client'
-import Link from 'next/link'
 import Header from '@/components/header'
 import DashboardDrawer from '@/components/dashboardDrawer'
-import { Button } from '@/components/ui/button'
-import { useRandomTextWithStyle } from '@/services/transitionHooks'
 import DisplayQuizTemplate from '@/components/display-quiz-template'
 
 const Dashboard = () => {
-  const transitionDuration = 3000
-  const ob = {
-    'take quiz': 'text-semibold text-red-500 p-15',
-    'hello there': 'text-semibold text-white p-15',
-    'beep boop': 'text-extrabold text-blue-400 p-2',
-  }
-  const { text, style } = useRandomTextWithStyle(transitionDuration, ob)
-
   return (
     <main className='flex h-screen w-full flex-col'>
       <Header />
+      <DashboardDrawer className='content-start' />
       <DisplayQuizTemplate />
-      <DashboardDrawer />
     </main>
   )
 }
--- components/dashboardDrawer.js
+++ components/dashboardDrawer.js
@@ -7,14 +7,18 @@ import {
   DrawerTrigger,
 } from '@/components/ui/drawer'
 import { Button } from './ui/button'
-import { CountdownTimerIcon, GearIcon } from '@radix-ui/react-icons/dist'
+import { CountdownTimerIcon, DashboardIcon, GearIcon } from '@radix-ui/react-icons/dist'
 import SettingsModal from './settings-modal'
 import HistoryModal from './history-modal'
 
 const DashboardDrawer = () => {
   return (
     <Drawer direction='left'>
-      <DrawerTrigger>Open</DrawerTrigger>
+      <DrawerTrigger asChild>
+        <Button variant='outline' size='icon'>
+          <DashboardIcon className='h-4 w-4' />
+        </Button>
+      </DrawerTrigger>
       <DrawerContent className='min-h-full w-2/6 lg:w-1/5 '>
         <div className='flex h-full w-full flex-col items-start space-y-8 pl-2'>
           <DrawerHeader className='w-full'>
