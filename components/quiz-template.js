d1013ae3df3e1904831e16da1dcde362554b660e
G00398070 <G00398070@atu.ie>
2024-04-04 20:09:51 +0100

    homepage and dashboard compatible, added logo
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- components/quiz-template.js
+++ components/quiz-template.js
@@ -59,34 +59,32 @@ const QuizTemplate = () => {
   return (
     <div
       id='main-container'
-      className='flex min-h-screen min-w-full flex-col content-between items-start lg:items-center lg:justify-around lg:pb-32'>
-      <>
-        <div id='question' className='min-h-full w-full py-6 text-2xl lg:w-4/5'>
-          <p className='text-lg'>Q{activeQuestionNo + 1}</p>
-          <Card className='lg:h-24 lg:text-center'>{activeQuestion}</Card>
-        </div>
-        <div
-          id='options'
-          className='flex w-full flex-col items-start space-y-10 lg:flex-row lg:flex-wrap lg:items-end lg:justify-evenly lg:space-y-16'>
-          {quizArray[activeQuestionNo].options.map((option, index) => (
-            <Button
-              variant={index === selectedIndex ? 'mcq' : 'default'}
-              key={option}
-              onClick={() => handleOptionSelect(option, index)}
-              className='min-h-fit w-full text-lg lg:w-5/12 lg:flex-none'>
-              {option}
-            </Button>
-          ))}
-        </div>
-        <div className='flex flex-row self-end pt-9 lg:self-center'>
+      className='flex min-h-screen min-w-full flex-col content-between  lg:items-center lg:justify-around lg:pb-32'>
+      <div id='question' className='min-h-full w-full py-6 text-2xl lg:w-4/5'>
+        <p className='text-lg'>Q{activeQuestionNo + 1}</p>
+        <Card className='lg:h-24 lg:text-center'>{activeQuestion}</Card>
+      </div>
+      <div
+        id='options'
+        className='flex w-full flex-col items-start space-y-10 lg:flex-row lg:flex-wrap lg:items-end lg:justify-evenly lg:space-y-16'>
+        {quizArray[activeQuestionNo].options.map((option, index) => (
           <Button
-            onClick={isLastQuestion ? () => onClickFinish() : () => onClickNext()}
-            disabled={selectedOption === null}>
-            <PlayIcon className='mr-2 h-4 w-4' />
-            {!isLastQuestion ? 'Next' : 'Finish'}
+            variant={index === selectedIndex ? 'mcq' : 'default'}
+            key={option}
+            onClick={() => handleOptionSelect(option, index)}
+            className='min-h-fit w-full text-lg lg:w-5/12 lg:flex-none '>
+            {option}
           </Button>
-        </div>
-      </>
+        ))}
+      </div>
+      <div className='flex flex-row self-end pt-9 lg:self-center'>
+        <Button
+          onClick={isLastQuestion ? () => onClickFinish() : () => onClickNext()}
+          disabled={selectedOption === null}>
+          <PlayIcon className='mr-2 h-4 w-4' />
+          {!isLastQuestion ? 'Next' : 'Finish'}
+        </Button>
+      </div>
     </div>
   )
 }
